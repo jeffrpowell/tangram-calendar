86db5f907d8fb84c04150e0b9460b525e1e4f9ce
jeffrpowell <jeffr.powell@outlook.com>
2022-02-04 11:34:13 -0700

    encoded static locations of the grid

--- src/main/java/dev/jeffrpowell/Grid.java
+++ src/main/java/dev/jeffrpowell/Grid.java
@@ -1,6 +1,7 @@
 package dev.jeffrpowell;
 
 import java.awt.geom.Point2D;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,11 +10,17 @@ public class Grid {
     private final List<Piece> allPieces;
     private final Map<Point2D, Piece> placedPieces;
     private final Map<Point2D, Boolean> covered;
-
+    
     public Grid(List<Piece> allPieces) {
         this.allPieces = allPieces;
         this.placedPieces = new HashMap<>();
         this.covered = new HashMap<>();
+    }
+
+    public void registerTargetDate(LocalDate d) {
+        covered.put(GridConstants.getMonthLocation(d.getMonth()), true);
+        covered.put(GridConstants.getDayLocation(d.getDayOfMonth()), true);
+        covered.put(GridConstants.getDayOfWeekLocation(d.getDayOfWeek()), true);
     }
 
     public boolean tryToFindSolution() {
---
+++ src/main/java/dev/jeffrpowell/GridConstants.java
@@ -0,0 +1,92 @@
+package dev.jeffrpowell;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.geom.Point2D;
+
+public class GridConstants {
+    private GridConstants() {}
+
+    private static final EnumMap<Month, Point2D> MONTH_LOCATIONS;
+    private static final Map<Integer, Point2D> DAY_LOCATIONS;
+    private static final EnumMap<DayOfWeek, Point2D> DAY_OF_WEEK_LOCATIONS;
+
+    static {
+        //row 0
+        MONTH_LOCATIONS = new EnumMap<>(Month.class);
+        MONTH_LOCATIONS.put(Month.JANUARY, new Point2D.Double(0, 0));
+        MONTH_LOCATIONS.put(Month.FEBRUARY, new Point2D.Double(1, 0));
+        MONTH_LOCATIONS.put(Month.MARCH, new Point2D.Double(2, 0));
+        MONTH_LOCATIONS.put(Month.APRIL, new Point2D.Double(3, 0));
+        MONTH_LOCATIONS.put(Month.MAY, new Point2D.Double(4, 0));
+        MONTH_LOCATIONS.put(Month.JUNE, new Point2D.Double(5, 0));
+        //row 1
+        MONTH_LOCATIONS.put(Month.JULY, new Point2D.Double(0, 1));
+        MONTH_LOCATIONS.put(Month.AUGUST, new Point2D.Double(1, 1));
+        MONTH_LOCATIONS.put(Month.SEPTEMBER, new Point2D.Double(2, 1));
+        MONTH_LOCATIONS.put(Month.OCTOBER, new Point2D.Double(3, 1));
+        MONTH_LOCATIONS.put(Month.NOVEMBER, new Point2D.Double(4, 1));
+        MONTH_LOCATIONS.put(Month.DECEMBER, new Point2D.Double(5, 1));
+        //row 2
+        DAY_LOCATIONS = new HashMap<>();
+        DAY_LOCATIONS.put(1, new Point2D.Double(0, 2));
+        DAY_LOCATIONS.put(2, new Point2D.Double(1, 2));
+        DAY_LOCATIONS.put(3, new Point2D.Double(2, 2));
+        DAY_LOCATIONS.put(4, new Point2D.Double(3, 2));
+        DAY_LOCATIONS.put(5, new Point2D.Double(4, 2));
+        DAY_LOCATIONS.put(6, new Point2D.Double(5, 2));
+        DAY_LOCATIONS.put(7, new Point2D.Double(6, 2));
+        //row 3
+        DAY_LOCATIONS.put(8, new Point2D.Double(0, 3));
+        DAY_LOCATIONS.put(9, new Point2D.Double(1, 3));
+        DAY_LOCATIONS.put(10, new Point2D.Double(2, 3));
+        DAY_LOCATIONS.put(11, new Point2D.Double(3, 3));
+        DAY_LOCATIONS.put(12, new Point2D.Double(4, 3));
+        DAY_LOCATIONS.put(13, new Point2D.Double(5, 3));
+        DAY_LOCATIONS.put(14, new Point2D.Double(6, 3));
+        //row 4
+        DAY_LOCATIONS.put(15, new Point2D.Double(0, 4));
+        DAY_LOCATIONS.put(16, new Point2D.Double(1, 4));
+        DAY_LOCATIONS.put(17, new Point2D.Double(2, 4));
+        DAY_LOCATIONS.put(18, new Point2D.Double(3, 4));
+        DAY_LOCATIONS.put(19, new Point2D.Double(4, 4));
+        DAY_LOCATIONS.put(20, new Point2D.Double(5, 4));
+        DAY_LOCATIONS.put(21, new Point2D.Double(6, 4));
+        //row 5
+        DAY_LOCATIONS.put(22, new Point2D.Double(0, 5));
+        DAY_LOCATIONS.put(23, new Point2D.Double(1, 5));
+        DAY_LOCATIONS.put(24, new Point2D.Double(2, 5));
+        DAY_LOCATIONS.put(25, new Point2D.Double(3, 5));
+        DAY_LOCATIONS.put(26, new Point2D.Double(4, 5));
+        DAY_LOCATIONS.put(27, new Point2D.Double(5, 5));
+        DAY_LOCATIONS.put(28, new Point2D.Double(6, 5));
+        //row 6
+        DAY_LOCATIONS.put(29, new Point2D.Double(0, 6));
+        DAY_LOCATIONS.put(30, new Point2D.Double(1, 6));
+        DAY_LOCATIONS.put(31, new Point2D.Double(2, 6));
+        DAY_OF_WEEK_LOCATIONS = new EnumMap<>(DayOfWeek.class);
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(3, 6));
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(4, 6));
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(5, 6));
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(6, 6));
+        //row 7
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(4, 7));
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(5, 7));
+        DAY_OF_WEEK_LOCATIONS.put(DayOfWeek.SUNDAY, new Point2D.Double(6, 7));
+    }
+
+    public static Point2D getMonthLocation(Month m) {
+        return MONTH_LOCATIONS.get(m);
+    }
+
+    public static Point2D getDayLocation(int d) {
+        return DAY_LOCATIONS.get(d);
+    }
+
+    public static Point2D getDayOfWeekLocation(DayOfWeek dow) {
+        return DAY_OF_WEEK_LOCATIONS.get(dow);
+    }
+}
