e255cc14470aa5c5f19d7e9d5975f90a7b5b4844
jeffrpowell <jeffr.powell@outlook.com>
2023-01-25 22:46:18 -0700

    added new Cloudflare mode to submit to Cloudflare worker cache solution

--- src/main/java/dev/jeffrpowell/App.java
+++ src/main/java/dev/jeffrpowell/App.java
@@ -1,6 +1,17 @@
 package dev.jeffrpowell;
 
 import java.awt.geom.Point2D;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,26 +19,38 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class App 
+public class App
 {
+    static final String CF_BASE_URL = "https://tangram-calendar-submit.jeffpowell.dev/";
+    static final Function<LocalDate, URI> CF_HINT_URI = d -> URI.create(CF_BASE_URL + "hint/" + d);
+    static final Function<LocalDate, URI> CF_SOLUTION_URI = d -> URI.create(CF_BASE_URL + "solution/" + d);
+    static final String MAX_DATE_FILENAME = "maxDate";
     static int attempts = 0;
     static long startTime = 0L;
     static Mode mode = Mode.SOLUTION;
     static Set<GridBranch> solutions = new HashSet<>();
-    enum Mode {SOLUTION("solution"), HINT("hint");
+    static HttpClient httpClient = HttpClient.newHttpClient();
+
+    enum Mode {SOLUTION("solution"), HINT("hint"), CLOUDFLARE("cloudflare");
         String print;
         Mode(String print) {
             this.print = print;
         }
 
         static Mode fromArgs(String[] args) {
-            if (args.length > 0 && args[0].equals("hint")) {
-                return HINT;
+            if (args.length > 0) {
+                if (args[0].equals("hint")) {
+                    return HINT;
+                }
+                else if (args[0].equals("cloudflare")) {
+                    return CLOUDFLARE;
+                }
             }
             return SOLUTION;
         }
@@ -37,15 +60,40 @@ public class App
             return print;
         }
     }
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
         System.out.println("TANGRAM CALENDAR SOLVER");
         mode = Mode.fromArgs(args);
         System.out.println("Running in " + mode + " mode");
-        solve(5);
+        if (mode == Mode.CLOUDFLARE) {
+            LocalDate maxDate = LocalDate.now();
+            try(Scanner scanner = new Scanner(new File(MAX_DATE_FILENAME))){
+                if (scanner.hasNext()) {
+                    String maxDateStr = scanner.next();
+                    maxDate = LocalDate.parse(maxDateStr);
+                    System.out.println("Found maxDate file.");
+                }
+            }
+            for (int i = 0; i < 7; i++) {
+                maxDate = maxDate.plusDays(1);
+                solutions.clear();
+                solve(5, maxDate);
+                try (PrintWriter out = new PrintWriter(MAX_DATE_FILENAME)) {
+                    out.print(maxDate);
+                }
+            }
+        }
+        else {
+            solve(5, LocalDate.now());
+        }
     }
 
-    private static void solve(int hintThreshold) {
+    private static List<String> inputStreamToStringList(InputStream inputStream) {
+		return new BufferedReader(new InputStreamReader(inputStream))
+			.lines().collect(Collectors.toList());
+	}
+
+    private static void solve(int hintThreshold, LocalDate targetDate) throws IOException, InterruptedException{
         List<List<Piece>> pieces = PieceFactory.createPieces().stream()
             .map(PieceFactory::generateDerivativePieces)
             .collect(Collectors.toList());
@@ -58,10 +106,9 @@ public class App
             IntStream.generate(() -> 0)
                 .limit(pieces.size())
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        LocalDate targetDate = LocalDate.now();
         System.out.println("Solving for " + targetDate + " (" + targetDate.getDayOfWeek() + ")");
         startTime = System.nanoTime();
-        while ((mode == Mode.SOLUTION && !splitPermutationNum.equals(maxIndexes)) || (mode == Mode.HINT && solutions.size() < hintThreshold)) {
+        while ((mode == Mode.SOLUTION && !splitPermutationNum.equals(maxIndexes)) || ((mode == Mode.HINT || mode == Mode.CLOUDFLARE) && solutions.size() < hintThreshold)) {
             attempts++;
             List<Piece> pieceChoices = new ArrayList<>();
             for (int i = 0; i < pieces.size(); i++) {
@@ -80,9 +127,9 @@ public class App
             System.out.println("Attempted rotation permutations (out of 8.3MM): " + attempts);
         }
         else {
-            boolean goodHintFound = produceHint();
+            boolean goodHintFound = produceHint(targetDate);
             if (!goodHintFound) {
-                solve(hintThreshold + 3);
+                solve(hintThreshold + 3, targetDate);
             }
         }
     }
@@ -129,7 +176,7 @@ public class App
         }
     }
 
-    private static boolean produceHint() {
+    private static boolean produceHint(LocalDate targetDate) throws IOException, InterruptedException{
         Map<TranslatedPiece, Long> allSolvedPieces = solutions.stream().map(GridBranch::getSolutionPieces).flatMap(List::stream).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         List<TranslatedPiece> frequentSolutionPieces = allSolvedPieces.entrySet().stream().filter(e -> e.getValue() > 2).map(Map.Entry::getKey).collect(Collectors.toList());
         Set<Point2D> committedPts = new HashSet<>();
@@ -154,8 +201,34 @@ public class App
                 printInstructions.put(pt, Integer.toString(i));
             }
         }
-        System.out.println("\nToday's hint:");
-        System.out.println(Point2DUtils.pointsToString(printInstructions));
+        String hint = Point2DUtils.pointsToString(printInstructions);
+        if (mode == Mode.CLOUDFLARE) {
+            System.out.println("Sending POST " + CF_HINT_URI.apply(targetDate));
+            HttpRequest hintRequest = HttpRequest.newBuilder()
+                .uri(CF_HINT_URI.apply(targetDate))
+                .POST(BodyPublishers.ofString(hint))
+                .build();
+            httpClient.send(hintRequest, BodyHandlers.ofString());
+            List<TranslatedPiece> solutionPieces = solutions.stream().findAny().get().getSolutionPieces();
+            printInstructions = new HashMap<>();
+            for (int i = 0; i < solutionPieces.size(); i++) {
+                TranslatedPiece p = solutionPieces.get(i);
+                for (Point2D pt : p.getLocations()) {
+                    printInstructions.put(pt, Integer.toString(i));
+                }
+            }
+            String solution = Point2DUtils.pointsToString(printInstructions);
+            System.out.println("Sending POST " + CF_SOLUTION_URI.apply(targetDate));
+            HttpRequest solutionRequest = HttpRequest.newBuilder()
+                .uri(CF_SOLUTION_URI.apply(targetDate))
+                .POST(BodyPublishers.ofString(solution))
+                .build();
+            httpClient.send(solutionRequest, BodyHandlers.ofString());
+        }
+        else {
+            System.out.println("\nToday's hint:");
+            System.out.println(hint);
+        }
         return true;
     }
 }
